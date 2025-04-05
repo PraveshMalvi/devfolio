8575c0f7f0e8b6d122c2946fd4f562fd24d81eb2
PraveshMalvi <praveshmalvi009@gmail.com>
2025-04-05 12:23:06 +0530

    added jest & react testing library

---
+++ src/pages/Home/Home.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Home from './Home';
+
+test('renders the welcome header', () => {
+    render(<Home />);
+    const heading = screen.getByText(/Welcome to Devfolio/i);
+    expect(heading).toBeInTheDocument();
+  });
