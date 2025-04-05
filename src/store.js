eb7915be311710406c22707bfd236fee8514a874
PraveshMalvi <praveshmalvi009@gmail.com>
2025-04-05 12:45:50 +0530

    ran lint and test

--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
-import todoReducer from "./features/todoSlice"
-import userReducer from "./features/userSlice"
-import counterReducer from "./features/counterSlice"
+import { configureStore } from '@reduxjs/toolkit';
+import todoReducer from './features/todoSlice';
+import userReducer from './features/userSlice';
+import counterReducer from './features/counterSlice';
 
 export const store = configureStore({
-    reducer: {
-        todo: todoReducer,
-        user: userReducer,
-        counter: counterReducer
-    }
-})
+  reducer: {
+    todo: todoReducer,
+    user: userReducer,
+    counter: counterReducer,
+  },
+});
--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,19 +1,24 @@
 module.exports = {
-    extends: [
-      'stylelint-config-standard',
-      'stylelint-config-tailwindcss',
-      // 'stylelint-config-prettier',
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-tailwindcss',
+    // 'stylelint-config-prettier',
+  ],
+  plugins: ['stylelint-order'],
+  ignoreFiles: [
+    '**/*.js',
+    '**/*.jsx',
+    '**/*.ts',
+    '**/*.tsx',
+    '**/*.json',
+  ],
+  rules: {
+    'order/properties-order': [],
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind', 'apply', 'variants', 'responsive', 'screen'],
+      },
     ],
-    plugins: ['stylelint-order'],
-    rules: {
-      // Optional: Custom rules if you want
-      'order/properties-order': [],
-      'at-rule-no-unknown': [
-        true,
-        {
-          ignoreAtRules: ['tailwind', 'apply', 'variants', 'responsive', 'screen'],
-        },
-      ],
-    },
-  }
-  
+  },
+};
